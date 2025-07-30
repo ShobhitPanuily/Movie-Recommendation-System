2afc87bb265e6cecbcbc4992708e3ea644753d1d
Shobhit Panuily <126354398+ShobhitPanuily@users.noreply.github.com>
2025-07-30 23:00:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def get_recommendations(title, cosine_sim=cosine_sim):
     idx = movies[movies['title'] == title].index[0]
     sim_scores = list(enumerate(cosine_sim[idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:11]  # Top 10 excluding the selected one
+    sim_scores = sim_scores[1:11]
     movie_indices = [i[0] for i in sim_scores]
     return movies['title'].iloc[movie_indices]
 
